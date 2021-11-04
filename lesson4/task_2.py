cbeb71f0a687796fa8251e12534fc4b7de7157e7
nedobolsky <92752290+nedobolsky@users.noreply.github.com>
2021-11-04 17:24:40 +0300

    Delete lesson4 directory

--- lesson4/task_2.py
+++
@@ -1,3 +0,0 @@
-data = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55, 800]
-my_data = [data[el] for el in range(len(data) - 1) if data[el] > data[el - 1]]
-print(my_data)
--- lesson4/task_3.py
+++
@@ -1,2 +0,0 @@
-my_data = [el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]
-print(my_data)
